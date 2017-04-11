3ffb52f6e8cb421f490c946a8e250938dfe67360
Codetector <codetector@codetector.cn>
2017-04-10 20:14:39 -0400

    Initial Commit

---
+++ Fredit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Fredit
+//
+//  Created by Codetector on 2017/4/10.
+//  Copyright © 2017年 GuardianTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Fredit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fredit
+//
+//  Created by Codetector on 2017/4/10.
+//  Copyright © 2017年 GuardianTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
