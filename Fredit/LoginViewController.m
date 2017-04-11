06bdaeddc1325a7868855ffee280c5013da16f28
Codetector <codetector@codetector.cn>
2017-04-10 20:50:29 -0400

    Ok Welcome Objective-C

--- Fredit/LoginViewController.m
+++ Fredit/LoginViewController.m
@@ -6,19 +6,25 @@
 //  Copyright © 2017年 GuardianTechnologies. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
+#import "UIView+RoundCornerUIView.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
+    @property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void) viewDidLayoutSubviews {
+    [[self signUpButton]createRoundCorner:7 shadowRadius:0 borderWidth: 0.4 shadowOpacity:0];
+    [super viewDidLayoutSubviews];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
