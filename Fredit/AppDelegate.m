effacd2ca4c2b9671dbcea6913d43521eb3fb4c4
Codetector <codetector@codetector.cn>
2017-04-20 11:04:30 -0400

    initial Sync

--- Fredit/AppDelegate.m
+++ Fredit/AppDelegate.m
@@ -18,7 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [CoreDataSyncorization sharedSyncorization];
+    [[CoreDataSyncorization sharedSyncorization]attemptFullSyncorization:nil];
     // Override point for customization after application launch.
     return YES;
 }
